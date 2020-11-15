03d17f4684e2900a7ecdb27cf46fd8e6a6c45b52
Daniel Persson <persson.daniel.1990@gmail.com>
2020-11-15 17:38:01 +0100

    Fix sql lite database setup

---
+++ database-test.js
@@ -0,0 +1,19 @@
+import { open } from 'sqlite'
+import sqlite3 from 'sqlite3'
+
+
+(async () => {
+    const [db] = await Promise.all([
+        open({
+        filename: '/tmp/database.db',
+        driver: sqlite3.Database
+        }),
+    ])
+    await db.migrate({
+        force: true, 
+        migrationsPath: './migrations'
+    })
+
+    const people = await db.all('SELECT * FROM person');
+    console.log("All people", JSON.stringify(people, null, 2))
+})()
--- migrations/001-helloworld.sql
+++ migrations/001-helloworld.sql
@@ -1,17 +1,29 @@
+--------------------------------------------------------------------------------
 -- Up
-CREATE TABLE Person(
+--------------------------------------------------------------------------------
+
+CREATE TABLE person (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT
-)
+);
 
 CREATE TABLE vehicle (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     brand TEXT,
     model TEXT,
-    ownerId INTEGER FOREIGN KEY REFERENCES Person(id)
-)
+    ownerId INTEGER REFERENCES Person(id)
+);
 
+INSERT INTO person (name, email) values ("Daniel", "Danielperra@gmail.com");
+INSERT INTO person (name, email) values ("Jack", "Jackperra@gmail.com");
+
+INSERT INTO vehicle (brand, model, ownerId) values("audi", "R8", 1);
+INSERT INTO vehicle (brand, model, ownerId) values("mercedes", "benz", 1);
+
+--------------------------------------------------------------------------------
 -- Down
+--------------------------------------------------------------------------------
+
 DROP TABLE Person;
 DROP TABLE Vehicle;
